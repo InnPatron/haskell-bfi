0ee172f32495fe5f74fd8f632690a75e5c4c4f63
Raptor21 <Raptor21>
2019-05-06 22:29:40 +0100

    Implement helper functions

--- bf.hs
+++ bf.hs
@@ -1,5 +1,5 @@
 data VM = Exec (State (Maybe Instruction)) (State Integer)
-          End State Integer
+        | End (State Integer)
 
 data State t = State [t] t [t]
     deriving Show
@@ -13,6 +13,41 @@ data Instruction = IncDP
                  | JmpFwd
                  | JmpBack
     deriving Show
+
+shiftR :: State a -> State a
+shiftR (State left current (rightFirst:rightRest)) =
+    State (current : left) rightFirst rightRest 
+shiftR (State left current []) =
+    error "Shifted right beyond the tape"
+
+shiftL (State (leftFirst: leftRest) current right ) =
+    State leftRest leftFirst (current : right)
+shiftL (State [] current right) =
+    error "Shifted left beyond the tape"
+
+stateApply :: (a -> a) -> (State a) -> (State a)
+stateApply f (State left current right) = 
+    State left (f current) right
+
+current :: State a -> a
+current (State _ current _) = current
+
+jumpForward :: State Instruction -> State Instruction
+jumpForward state =
+    let nextState = shiftR state
+
+     in case (current nextState) of 
+          JmpBack   -> nextState
+          otherwise -> jumpForward nextState
+
+jumpBackward :: State Instruction -> State Instruction
+jumpBackward state =
+    let nextState = shiftL state
+
+     in case (current nextState) of
+          JmpFwd    -> nextState
+          otherwise -> jumpBackward nextState
+
 
 initState :: String -> Int -> (State Instruction, State Integer)
 initState string size = 
